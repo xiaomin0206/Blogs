83988045d664be48d38cdc16ca1d6d4c4e70c972
邹晓敏 <2304160871@qq.com>
2021-02-03 16:56:32 +0800

    deploy

--- .temp/app-enhancers/global-components-2.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.component("Badge", () => import("E:\\文档\\vuepress-demo\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
-Vue.component("CodeBlock", () => import("E:\\文档\\vuepress-demo\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
-Vue.component("CodeGroup", () => import("E:\\文档\\vuepress-demo\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
-
-
-export default {}
